42aca2edf9a50f4f4fef1956791d351be5c12d3e
Gulduster <102225441+Gulduster@users.noreply.github.com>
2024-08-05 14:12:13 -0300

    sex

--- showroom_app-hive-doble-release/lib/pages/scanner_search.dart
+++ showroom_app-hive-doble-release/lib/pages/scanner_search.dart
@@ -227,48 +227,47 @@ Widget build(BuildContext context) {
           ),
           ]else ... [
             Container(
-                  height: MediaQuery.of(context).size.height /9,
-                  width: MediaQuery.of(context).size.width/1.1,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                  
-                      alignment: Alignment.center,
-                      backgroundColor: MaterialStatePropertyAll(colors.primary)
-                    ),
-                    onPressed: () async {
-                      var res = await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              const SimpleBarcodeScannerPage(),
-                        ),
-                      );
-                      arrancarContador = false;
-                      if (res is String) {
-                        result = res;
-                        if (result != '-1'){
-                          productoScanner = await ProductServices().getProductsOfflineFinal(result);
-                          
-                          context.read<ItemProvider>().setProduct(productoScanner[0]);
-                          context.read<ItemProvider>().setItem(productoScanner[0].raiz);
-                          historial.insert(0, productoScanner[0]);
-                          arrancarContador = false;
-                          appRouter.push('/product_page');    
-                        }
-                         
-                      }
-                      setState(() {
-                        
-                      });
-                    },
-                    child: const Text(
-    
-                      'Escanee Un Producto con su telefono',textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 22, color: Colors.white, ), 
-                      
-                    ),
-                  ),
+              height: MediaQuery.of(context).size.height /9,
+              width: MediaQuery.of(context).size.width/1.1,
+              child: ElevatedButton(
+                style: ButtonStyle(
+              
+                  alignment: Alignment.center,
+                  backgroundColor: WidgetStatePropertyAll(colors.primary)
                 ),
+                onPressed: () async {
+                  var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ),
+                  );
+                  if (res == null) {
+                    print("El scanner se cerro o no se tiene permisos de cámara");
+                  }
+                  if (res is String) {
+                    result = res;
+                    if (result != '-1'){
+                      productoScanner = await ProductServices().getProductsOfflineFinal(result);
+                      context.read<ItemProvider>().setProduct(productoScanner[0]);
+                      context.read<ItemProvider>().setItem(productoScanner[0].raiz);
+                      historial.insert(0, productoScanner[0]);
+                      appRouter.push('/product_page');    
+                    }
+                     
+                  }
+                  setState(() {
+                    
+                  });
+                },
+                child: const Text(
+
+                  'Escanee Un Producto con su telefono',textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 22, color: Colors.white, ), 
+                  
+                ),
+              ),
+            ),
                 const SizedBox(
                   height: 16,
                 ),
